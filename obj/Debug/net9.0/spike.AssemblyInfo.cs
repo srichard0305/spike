7fd956fd53e48bcc14e92f496c2e129cf525e9f0
Steve <steve.richard3592@gmail.com>
2025-01-24 16:33:05 -0800

    added sided bar view and styles

--- obj/Debug/net9.0/spike.AssemblyInfo.cs
+++ obj/Debug/net9.0/spike.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spike")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22abb979c8baf0d3529f26e40dae524423137d24")]
 [assembly: System.Reflection.AssemblyProductAttribute("spike")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spike")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
